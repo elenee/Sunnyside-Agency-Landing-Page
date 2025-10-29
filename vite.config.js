50a31e398a6bc6ff04cc67fe3f16cdbd0c5a8f10
elenee <elenerainauli@gmail.com>
2025-10-29 14:46:27 +0400

    Fix Vite config for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Sunnyside-Agency-Landing-Page/',
   plugins: [
     react({
       babel: {
